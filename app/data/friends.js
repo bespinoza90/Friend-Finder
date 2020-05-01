76d7d065e1cf86c8d29e6b2f94b14335b6a53f15
Bobby Espinoza <bobbyespinoza@gmail.com>
2020-05-01 12:29:57 -0700

    added data

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [ 
+    {
+        "name": "Jerry Paper",
+        "photo": "https://images.app.goo.gl/S595h5EAm4bCNgKq7",
+        "scores": [
+            5,
+            1,
+            5,
+            1,
+            1,
+            1,
+            3,
+            5,
+            5,
+            5,
+        ]
+    },
+    {
+        "name": "Stimulator Jones",
+        "Photo": "https://images.app.goo.gl/cx1UXnC9yprP8pet9",
+        "scores": [
+            5,
+            5,
+            1,
+            2,
+            1,
+            4,
+            4,
+            1,
+            3,
+            3,
+        ]
+    },
+
+    { 
+        "name": "Sudan Archives",
+        "photo": "https://images.app.goo.gl/3kVi9LqkjBK1Hb1L6",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            2,
+            3,
+            4,
+            4,
+            2,
+            2,
+
+        ]
+        
+    },
+]
